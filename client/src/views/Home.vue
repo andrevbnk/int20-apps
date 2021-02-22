77bc29d737bf6d619e01fce3cb3be48ec3be1e4b
Andrey <andrevbnk72@gmail.com>
2021-02-22 15:36:19 +0200

    Fixed Filter Id Animal

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -38,10 +38,10 @@
             <div class="d-flex flex-wrap align-center mr-auto">
               <div
                 class="card-animal"
-                v-for="(n,index) in sortFilterAnimals"
+                v-for="(n) in sortFilterAnimals"
                 :key="n.description"
               >
-                <Card :id="index" :animal="n" />
+                <Card :animal="n" />
                 
               </div>
             </div>
@@ -92,6 +92,7 @@ export default {
     animals: [
       {
         title: "Dog Haski",
+        id:'0',
         city: "Kiev",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -109,6 +110,7 @@ export default {
       {
         title: "Cat 'Oliver' ",
         city: "Kiev",
+        id:'1',
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         animalType: "Cat",
@@ -127,6 +129,7 @@ export default {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         animalType: "Fox",
+        id:'2',
         animalSize: "s",
         animalColor: "black",
         city: "Kharkiv",
@@ -142,6 +145,7 @@ export default {
       {
         title: "Vasia mastif",
         city: "Odesa",
+        id:'3',
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         animalType: "Cat",
@@ -159,6 +163,7 @@ export default {
       {
         title: "Lost Good Boy 'Sharisk' ",
         city: "Odesa",
+        id:'4',
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         animalType: "Dog",
